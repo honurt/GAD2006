862d103af44d39a8756a4ffe45876f0fdc063485
OnurTercan <118351671+honurt@users.noreply.github.com>
2025-01-03 03:41:07 +0300

    remove

--- Homework2/Source/Homework2/Homework2.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Homework2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Homework2, "Homework2" );
--- LearningCPP/Source/LearningCPP/Private/COAGameState.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "COAGameState.h"
-
-ACOAGameState::ACOAGameState()
-{
-	Points = 0;
-}
